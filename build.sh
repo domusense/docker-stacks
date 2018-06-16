15146c3f998384aa1aad84f911ea93d428c98d45
domusense <37619939+domusense@users.noreply.github.com>
2018-06-17 01:52:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run -i -t -p 10000:8888 -e JUPYTER_LAB_ENABLE=yes -v "/jupyter/":/home/jovyan/work jupyter/datascience-notebook:e5c5a7d3e52d
+docker run -i -t -p 10000:8888 -e JUPYTER_LAB_ENABLE=yes -v "/jupyter/":/home/jovyan/work jupyter/scipy-notebook:2c80cf3537ca
